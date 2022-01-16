7aff547433e4005920a3bc5bcf2e5b44c3081105
asmitpandey00 <asmit.pandey00@gmail.com>
2022-01-16 19:37:09 +0530

    first commit

---
+++ src/test/java/com/git/maven/MavenProject1/MavenProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.maven.MavenProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
